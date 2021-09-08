abf7ef850fa5c824dcc87a1ffc8bb8403560a7ac
Pooja Patel <poojapatel.web@gmail.com>
2021-09-08 12:17:10 -0400

    add some test for interviewrlist

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
+
 
 
 export default function InterviewerList(props) {
-console.log(props.interviewers);
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
   const interviewers = props.interviewers.map(interviewer => (
 
     <InterviewerListItem 
