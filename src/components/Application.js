1fe1c0c1c454aff3c4fdfbc78a21fb7d35ac7251
Pooja Patel <poojapatel.web@gmail.com>
2021-09-12 01:05:44 -0400

    remove unnecessary comments

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,57 +3,8 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import useApplicationData from "hooks/useApplicationData";
-
-
-
 import {getAppointmentsForDay, getInterview, getInterviewersForDay} from "helpers/selectors";
 
-// export default function Application(props) {
-//   const interviewers = getInterviewersForDay(state, state.day)
-//   const appointments = getAppointmentsForDay(state, state.day).map(appointment => {
-//     const interview = getInterview(state, appointment.interview)
-//     return (
-//       <Appointment
-//       {...appointment}
-//         key={appointment.id}
-//         // id={appointment.id}
-//         time={appointment.time}
-//         interview={interview}
-//         interviewers={interviewers}
-//         bookInterview={bookInterview}
-//         cancelInterview={cancelInterview}
-//       />
-//     )
-//   })
-//   return (
-//     <main className="layout">
-//       <section className="sidebar">
-//         <img
-//           className="sidebar--centered"
-//           src="images/logo.png"
-//           alt="Interview Scheduler"
-//         />
-//     <hr className="sidebar__separator sidebar--centered" />
-//     <nav className="sidebar__menu">
-//       <DayList
-//         days={state.days}
-//         day={state.day}
-//         setDay={setDay}
-//       />
-//     </nav>
-//     <img
-//       className="sidebar__lhl sidebar--centered"
-//       src="images/lhl.png"
-//       alt="Lighthouse Labs"
-//     />
-//     </section>
-//     <section className="schedule">
-//       {appointments}
-//       <Appointment key="last" time="5pm" bookInterview={bookInterview} cancelInterview={cancelInterview}/>
-//     </section>
-//     </main>
-//   );
-// }
 export default function Application(props) {
   const {
     state,
@@ -63,7 +14,6 @@ export default function Application(props) {
   } = useApplicationData();
 
   const interviewers = getInterviewersForDay(state, state.day);
-
   const appointments = getAppointmentsForDay(state, state.day).map(
     appointment => {
       return (
